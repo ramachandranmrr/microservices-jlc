6b48eaf0a435ddd871a2e45967927a885f94284c
Ramachandran R <52625499+ramachandranmrr@users.noreply.github.com>
2025-02-18 21:03:16 +0530

    e06-feign-eureka
    e06-feign-eureka

--- booksearch/src/main/java/com/jlcindia/booksearch/BookSearchController.java
+++ booksearch/src/main/java/com/jlcindia/booksearch/BookSearchController.java
@@ -3,14 +3,10 @@ package com.jlcindia.booksearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
@@ -19,7 +15,7 @@ public class BookSearchController {
 	static Logger log = LoggerFactory.getLogger(BookSearchController.class);
 	
 	@Autowired
-	LoadBalancerClient loadBalancerClient;
+	BookPriceProxy bookPriceProxy;
 	
 	@GetMapping("/mybook/{bookId}")
 	public BookInfo getBookById(@PathVariable Integer bookId) {
@@ -36,22 +32,11 @@ public class BookSearchController {
 		// Need to Invoke BookPriceMS
 		// Start Here
 		
-		ServiceInstance instance = loadBalancerClient.choose("bookprice");
-		String baseURL=instance.getUri().toString();
-		System.out.println("Base URL : "+ baseURL);
-		String apiURL = "/bookPrice/" + bookId;
-		String endpoint = baseURL + apiURL;
-		
-		RestTemplate restTemp=new RestTemplate();
-		ResponseEntity<BookPriceInfo> respEntity = 
-						restTemp.getForEntity(endpoint, BookPriceInfo.class);
-		
-		BookPriceInfo bookPriceInfo = respEntity.getBody();
-		
+		BookPriceInfo bookPriceInfo = bookPriceProxy.getBookPrice(bookId);
 		bookInfo.setPrice(bookPriceInfo.getPrice());
 		bookInfo.setOffer(bookPriceInfo.getOffer());
 		bookInfo.setServerPort(bookPriceInfo.getServerPort());
-
+		
 		// End Here
 		
 		return bookInfo;
