3bd1fc1468edac6f485d12642e25ade2b6eaceb7
Ramachandran R <52625499+ramachandranmrr@users.noreply.github.com>
2025-04-12 16:54:16 +0530

    cloud-gateway-circuitbreaker
    cloud-gateway-circuitbreaker

--- booksearch/src/main/java/com/jlcindia/booksearch/BookPriceProxy.java
+++ booksearch/src/main/java/com/jlcindia/booksearch/BookPriceProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("bookprice")
+@FeignClient(name = "bookprice")
 public interface BookPriceProxy {
 	
 	@GetMapping("/bookPrice/{bookId}")
--- booksearch/src/main/java/com/jlcindia/booksearch/BookSearchController.java
+++ booksearch/src/main/java/com/jlcindia/booksearch/BookSearchController.java
@@ -3,10 +3,18 @@ package com.jlcindia.booksearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @CrossOrigin
 @RestController
@@ -18,6 +26,8 @@ public class BookSearchController {
 	BookPriceProxy bookPriceProxy;
 	
 	@GetMapping("/mybook/{bookId}")
+	@Retry(name = "getInvoiceRetry")
+	@CircuitBreaker(name = "getInvoiceCB", fallbackMethod = "fallBookPrice")
 	public BookInfo getBookById(@PathVariable Integer bookId) {
 		log.info("---BookController---getBookById()-----");
 		
@@ -42,5 +52,21 @@ public class BookSearchController {
 		return bookInfo;
 		
 	}
+	
+	public BookInfo fallBookPrice(Integer bookId, Throwable t) {
+		return new BookInfo(bookId, null, null, null, null, 0, 0, "fallback-portttt");
+    }
+	
+	@GetMapping("/getMessage")
+    @RateLimiter(name = "getMessageRateLimit", fallbackMethod = "getMessageFallBack")
+    public ResponseEntity<String> getMessage(@RequestParam(value="name", defaultValue = "Hello") String name){
+
+        return ResponseEntity.ok().body("Message from getMessage() :" +name);
+    }
+
+    public ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+        .body("Too many requests : No further request will be accepted. Please try after sometime");
+    }
 
 }
