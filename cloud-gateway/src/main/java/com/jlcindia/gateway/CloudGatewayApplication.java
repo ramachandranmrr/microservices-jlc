c2ae0e92dfcd33eb6f38ff76154615636aa0950f
Ramachandran R <52625499+ramachandranmrr@users.noreply.github.com>
2025-04-15 21:06:54 +0530

    pom changes
    pom changes

--- cloud-gateway/src/main/java/com/jlcindia/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/jlcindia/gateway/CloudGatewayApplication.java
@@ -3,11 +3,9 @@ package com.jlcindia.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
